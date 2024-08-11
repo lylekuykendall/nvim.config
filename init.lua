98d8a110da50b96fc89099c632b479d934c828de
Lyle Kuykendall <l.kuykendall@fetchrewards.com>
2024-08-10 21:43:36 -0600

    debugger/go changes

--- init.lua
+++ init.lua
@@ -1,25 +1,25 @@
 -- Set <space> as the leader key
 -- See `:help mapleader`
 --  NOTE: Must happen before plugins are loaded (otherwise wrong leader will be used)
-vim.g.mapleader = ' '
-vim.g.maplocalleader = ' '
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
 
 -- Set to true if you have a Nerd Font installed and selected in the terminal
 vim.g.have_nerd_font = false
 
 -- [[ Setting options ]]
 -- this maps to file ./lua/options.lua
-require 'options'
+require("options")
 
 -- [[ Basic Keymaps ]]
 -- and this maps to ./lua/keymaps.lua
-require 'keymaps'
+require("keymaps")
 
 -- [[ Install `lazy.nvim` plugin manager ]]
-require 'lazy-bootstrap'
+require("lazy-bootstrap")
 
 -- [[ Configure and install plugins ]]
-require 'lazy-plugins'
+require("lazy-plugins")
 
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
