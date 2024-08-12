f7c4070597fa38d2208853125d4e1a10e1bdd61d
Lyle Kuykendall <l.kuykendall@fetchrewards.com>
2024-08-12 16:25:33 -0600

    added quickscope, updated rust plugins

--- lua/plugins/rust.lua
+++ lua/plugins/rust.lua
@@ -1,17 +1,26 @@
 return {
-  'simrat39/rust-tools.nvim',
-  config = function()
-    require('rust-tools').setup {
-      -- advanced configuration: https://github.com/simrat39/rust-tools.nvim?tab=readme-ov-file#configuration
-      server = {
-        on_attach = function(_, bufnr)
-          local rt = require 'rust-tools'
-          -- Hover actions
-          vim.keymap.set('n', '<C-space>', rt.hover_actions.hover_actions, { buffer = bufnr })
-          -- Code action groups
-          vim.keymap.set('n', '<leader>a', rt.code_action_group.code_action_group, { buffer = bufnr })
-        end,
-      },
-    }
-  end,
+	{
+		"simrat39/rust-tools.nvim",
+		config = function()
+			require("rust-tools").setup({
+				-- advanced configuration: https://github.com/simrat39/rust-tools.nvim?tab=readme-ov-file#configuration
+				server = {
+					on_attach = function(_, bufnr)
+						local rt = require("rust-tools")
+						-- Hover actions
+						vim.keymap.set("n", "<C-space>", rt.hover_actions.hover_actions, { buffer = bufnr })
+						-- Code action groups
+						vim.keymap.set("n", "<leader>a", rt.code_action_group.code_action_group, { buffer = bufnr })
+					end,
+				},
+			})
+		end,
+	},
+	{
+		"rust-lang/rust.vim",
+		ft = "rust",
+		init = function()
+			vim.g.rustfmt_autosave = 1 -- run rust formatter on save
+		end,
+	},
 }
