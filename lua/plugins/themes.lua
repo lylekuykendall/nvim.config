cea072f4e00934221fe2fcee3cf3b585593e0e5f
Lyle Kuykendall <l.kuykendall@fetchrewards.com>
2024-08-11 22:18:53 -0600

    dont need themes file

--- lua/plugins/themes.lua
+++
@@ -1,9 +0,0 @@
-return {
-  'EdenEast/nightfox.nvim',
-  'folke/tokyonight.nvim',
-  'rebelot/kanagawa.nvim',
-  'navarasu/onedark.nvim',
-  'Mofiqul/dracula.nvim',
-  'tiagovla/tokyodark.nvim',
-  'dgox16/oldworld.nvim',
-}
