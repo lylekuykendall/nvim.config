17c1c2312bcd767ea3605e849c4118d2950e0864
Lyle Kuykendall <l.kuykendall@fetchrewards.com>
2024-08-14 16:15:33 -0600

    move terminal keymaps to terminal file

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -46,26 +46,6 @@ vim.keymap.set("x", "K", ":move '<-2<CR>gv-gv", opts)
 vim.keymap.set("x", "<A-j>", ":move '>+1<CR>gv-gv", opts)
 vim.keymap.set("x", "<A-k>", ":move '<-2<CR>gv-gv", opts)
 
--- Terminal
-vim.keymap.set("n", "<A-h>", function()
-	require("nvterm.terminal").toggle("horizontal")
-end, { desc = "Toggle horizontal terminal" })
-vim.keymap.set("t", "<A-h>", function()
-	require("nvterm.terminal").toggle("horizontal")
-end, { desc = "Toggle horizontal terminal" })
-vim.keymap.set("n", "<A-v>", function()
-	require("nvterm.terminal").toggle("vertical")
-end, { desc = "Toggle vertical terminal" })
-vim.keymap.set("t", "<A-v>", function()
-	require("nvterm.terminal").toggle("vertical")
-end, { desc = "Toggle vertical terminal" })
-vim.keymap.set("n", "<A-.>", function()
-	require("nvterm.terminal").toggle("float")
-end, { desc = "Toggle floating terminal" })
-vim.keymap.set("t", "<A-.>", function()
-	require("nvterm.terminal").toggle("float")
-end, { desc = "Toggle floating terminal" })
-
 -- [[ Basic Autocommands ]]
 --  See `:help lua-guide-autocommands`
 
--- lua/plugins/terminal.lua
+++ lua/plugins/terminal.lua
@@ -1,6 +1,26 @@
 return {
-  'NvChad/nvterm',
-  config = function()
-    require('nvterm').setup()
-  end,
+	"NvChad/nvterm",
+	config = function()
+		-- Mappings
+		vim.keymap.set("n", "<A-h>", function()
+			require("nvterm.terminal").toggle("horizontal")
+		end, { desc = "Toggle horizontal terminal" })
+		vim.keymap.set("t", "<A-h>", function()
+			require("nvterm.terminal").toggle("horizontal")
+		end, { desc = "Toggle horizontal terminal" })
+		vim.keymap.set("n", "<A-v>", function()
+			require("nvterm.terminal").toggle("vertical")
+		end, { desc = "Toggle vertical terminal" })
+		vim.keymap.set("t", "<A-v>", function()
+			require("nvterm.terminal").toggle("vertical")
+		end, { desc = "Toggle vertical terminal" })
+		vim.keymap.set("n", "<A-.>", function()
+			require("nvterm.terminal").toggle("float")
+		end, { desc = "Toggle floating terminal" })
+		vim.keymap.set("t", "<A-.>", function()
+			require("nvterm.terminal").toggle("float")
+		end, { desc = "Toggle floating terminal" })
+
+		require("nvterm").setup()
+	end,
 }
