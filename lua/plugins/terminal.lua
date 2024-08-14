9f893ccc0e9e330af9f8987a64be5dd111e21d38
Lyle Kuykendall <l.kuykendall@fetchrewards.com>
2024-08-14 16:40:51 -0600

    swap nvterm for toggleterm

--- lua/plugins/terminal.lua
+++ lua/plugins/terminal.lua
@@ -1,26 +1,45 @@
 return {
-	"NvChad/nvterm",
+	"akinsho/toggleterm.nvim",
+	version = "*",
 	config = function()
-		-- Mappings
-		vim.keymap.set("n", "<A-h>", function()
-			require("nvterm.terminal").toggle("horizontal")
-		end, { desc = "Toggle horizontal terminal" })
-		vim.keymap.set("t", "<A-h>", function()
-			require("nvterm.terminal").toggle("horizontal")
-		end, { desc = "Toggle horizontal terminal" })
-		vim.keymap.set("n", "<A-v>", function()
-			require("nvterm.terminal").toggle("vertical")
-		end, { desc = "Toggle vertical terminal" })
-		vim.keymap.set("t", "<A-v>", function()
-			require("nvterm.terminal").toggle("vertical")
-		end, { desc = "Toggle vertical terminal" })
-		vim.keymap.set("n", "<A-.>", function()
-			require("nvterm.terminal").toggle("float")
-		end, { desc = "Toggle floating terminal" })
-		vim.keymap.set("t", "<A-.>", function()
-			require("nvterm.terminal").toggle("float")
-		end, { desc = "Toggle floating terminal" })
-
-		require("nvterm").setup()
+		vim.keymap.set(
+			{ "n", "t" },
+			"<A-v>",
+			"<CMD> ToggleTerm direction=vertical <CR>",
+			{ desc = "Toggle vertical terminal" }
+		)
+		vim.keymap.set(
+			{ "n", "t" },
+			"<A-h>",
+			"<CMD> ToggleTerm direction=horizontal <CR>",
+			{ desc = "Toggle horizontal terminal" }
+		)
+		vim.keymap.set(
+			{ "n", "t" },
+			"<A-.>",
+			"<CMD> ToggleTerm direction=float <CR>",
+			{ desc = "Toggle floating terminal" }
+		)
+		-- navigate in/out of terminal while keeping it open
+		function _G.set_terminal_keymaps()
+			local opts = { buffer = 0 }
+			vim.keymap.set("t", "<esc>", [[<C-\><C-n>]], opts)
+			vim.keymap.set("t", "jk", [[<C-\><C-n>]], opts)
+			vim.keymap.set("t", "<C-h>", [[<Cmd>wincmd h<CR>]], opts)
+			vim.keymap.set("t", "<C-j>", [[<Cmd>wincmd j<CR>]], opts)
+			vim.keymap.set("t", "<C-k>", [[<Cmd>wincmd k<CR>]], opts)
+			vim.keymap.set("t", "<C-l>", [[<Cmd>wincmd l<CR>]], opts)
+			vim.keymap.set("t", "<C-w>", [[<C-\><C-n><C-w>]], opts)
+		end
+		vim.cmd("autocmd! TermOpen term://*toggleterm#* lua set_terminal_keymaps()")
+		require("toggleterm").setup({
+			size = function(term)
+				if term.direction == "horizontal" then
+					return 15
+				elseif term.direction == "vertical" then
+					return vim.o.columns * 0.4
+				end
+			end,
+		})
 	end,
 }
